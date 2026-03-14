4d1907192ced01d90d7f2f9c9a995e529c5d18f7
markaskha-creator <mark.askha@gmail.com>
2026-03-14 11:30:16 +0300

    Созданы враги и проверка столкновений

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 class Sprite:
     def __init__(self, center, image):
@@ -33,6 +34,8 @@ image.fill('white')
 center = WIN_SIZE[0]/2 , WIN_SIZE[1]/2
 player = Sprite(center, image)
 bullets = []
+enemies = []
+score = 0
 
 running = True
 
@@ -50,15 +53,53 @@ while running:
             bullets.append(bullet)
 
 
+    if randint(0, 100) <= 3:
+
+        image = pygame.Surface((50, 50))
+        image.fill("green")
+        center = pygame.Vector2(player.rect.center)
+
+        r = randint(1, 4)
+        if r == 1:
+            pos = pygame.Vector2(randint(0, WIN_SIZE[0]), -100)
+        elif r == 2:
+            pos = pygame.Vector2(WIN_SIZE[0] + 100, randint(0, WIN_SIZE[1]))
+        elif r == 3:
+            pos = pygame.Vector2(randint(0, WIN_SIZE[0]), WIN_SIZE[1] + 100)
+        elif r == 4:
+            pos = pygame.Vector2(-100, randint(0, WIN_SIZE[1]))
+
+        direction = center - pos
+        enemy = MoveSprite(pos, image, randint(100, 400)/100, direction)
+        enemies.append(enemy)
 
     for bullet in bullets:
         bullet.update()
+    for enemy in enemies:
+        enemy.update()
+
+    for bullet in bullets:
+        for enemy in enemies:
+            if bullet.rect.coliderect(enemy.rect):
+                score += 1
+                bullets.remove(bullet)
+                enemies.remove(enemy)
+                break
+    for enemy in enemies:
+        if player.rect.coliderect(enemy.rect):
+            score = 0
+            enemies.clear()
+            bullets.clear()
+            break 
+           
 
     surface.fill("black")
     player.render(surface)
     for bullet in bullets:
         bullet.render(surface)
+    for enemy in enemies:
+        enemy.render(surface)
     window.flip()
 
     clock.tick(MAX_FPS)
-    print(clock.get_fps())
+
